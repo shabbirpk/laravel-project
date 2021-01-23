f6fdf7928ec686a30e4e93fae4e39b635bff1f84
= <shabbirahmed.dev@gmail.com>
2021-01-23 19:45:32 +0600

    Set up build a process

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,3 +14,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+elixir(function (mix) {
+    mix.sass('main.scss')
+        .coffee('module.coffee');
+});
